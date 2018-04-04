1bfe5c8f1251c87b79eedc63625ace7491a4e2f8
Bruno De Oliveira <bruno.de.oliveira@fisglobal.com>
2018-04-03 22:39:43 -0500

    Updated to iOS 11.0 - Added main funcionality

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,23 +7,44 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController{
     
-
+    var player: AVAudioPlayer!
+    
+    let sounds = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    let soundExt : String = "wav"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
-
+    
     @IBAction func notePressed(_ sender: UIButton) {
         
-        
+        playSound(sounds[sender.tag - 1])
         
     }
     
-  
-
+    func playSound(_ sound: String) {
+        
+        let soundURL = getNoteURL(sound)
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: soundURL)
+            
+            player.prepareToPlay()
+            player.play()
+            
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+    func getNoteURL(_ resourceName: String) -> URL {
+        return Bundle.main.url(forResource: resourceName, withExtension: soundExt)!
+    }
+    
+    
 }
-
